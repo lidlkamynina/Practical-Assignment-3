aca1e1d5d2e8e3162d0a3c3191dbd44d1b56c9b6
malle4aa <malichbogdan11@gmail.com>
2025-04-28 03:45:15 +0300

    Create thankyou.php

---
+++ pages/thankyou.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Thank You</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+
+<div class="container mt-5 text-center">
+    <h2>Thank You!</h2>
+    <p>Your message has been received. We will get back to you soon.</p>
+    <a href="index.php" class="btn btn-success mt-3">Return to Home</a>
+</div>
+
+</body>
+</html>
